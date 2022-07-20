1187068667ecb6525ff6e614a798cb22be83a7d8
Rafael da Silva Santos <rafasilva.9021@gmail.com>
2022-07-20 06:50:34 -0300

    Create template-strings.js

---
+++ javascriptParaBackEnd/javascriptTiposVariaveisFuncoes/template-strings.js
@@ -0,0 +1,9 @@
+const nome = "Rafael"
+const idade = 2022 - 1990;
+const cidadeDeNascimento = "Santos";
+
+//const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}.`;
+
+console.log(apresentacao)
