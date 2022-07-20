b19caecdf0d9ee87e7d2a35f829d8a1d51e08313
Rafael da Silva Santos <rafasilva.9021@gmail.com>
2022-07-20 07:08:46 -0300

    Create funcoes.js

---
+++ javascriptParaBackEnd/javascriptTiposVariaveisFuncoes/funcoes.js
@@ -0,0 +1,29 @@
+// let x = "";
+// console.log(x)
+// x = "oi";
+
+// DECLARAÇÃO DE FUNÇÃO
+
+// 1) declaração da função
+
+function imprimeTexto(texto) {
+    console.log(texto)
+}
+
+// 2) executa a função (1 ou mais vezes)
+
+imprimeTexto("oi");
+imprimeTexto("outro texto");
+
+// Três formas de escrever funções
+
+function soma () {
+    //outros códigos
+    //vários console.log()
+    return 2 + 2;
+}
+
+console.log(soma())
+
+
+imprimeTexto(soma());
