27dc3787c4fe597e79af8d92499b88a3a6db7989
Rafael da Silva Santos <rafasilva.9021@gmail.com>
2022-07-20 05:55:49 -0300

    Update conversoes.js

--- javascriptParaBackEnd/javascriptTiposVariaveisFuncoes/conversoes.js
+++ javascriptParaBackEnd/javascriptTiposVariaveisFuncoes/conversoes.js
@@ -18,7 +18,22 @@ console.log(numero + numeroString) //456456 concatenação por tipos diferentes
 
 console.log(numero + Number(numeroString)) /912
 
+let largura = "10";
+let altura = "5";
+console.log( + largura * + altura); 
+
+// teremos a conversão de String para números realizado usando o + antes das variáveis
+
 // Se a conversão for com uma string com letras, o retorno será NaN
 
 //String()
 
+let telefone = 12341234;
+console.log("O telefone é " + String(telefone));
+
+// teremos a conversão do número 12341234 para uma string “12341234” e assim poderemos fazer a concatenação entre as strings
+
+let telefone2 = 12341234;
+console.log("O telefone é " + telefone2.toString());
+
+// o .toString() é uma outra forma para  fazer essa conversão, que é mais parecida com outras linguagens de programação.
