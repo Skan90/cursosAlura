8c8f1ce35954f152ab07c1f0edf01a42818fdf41
Rafael da Silva Santos <rafasilva.9021@gmail.com>
2022-07-20 07:49:07 -0300

    Create expressoes-funcao.js

---
+++ javascriptParaBackEnd/javascriptTiposVariaveisFuncoes/expressoes-funcao.js
@@ -0,0 +1,26 @@
+//declaração de função
+
+function minhaFuncao(param) {
+    //bloco de código
+}
+
+// minhaFuncao("param")
+
+// expressão de função
+
+// const soma = function(num1, num2) {return num1 + num2}
+// console.log(soma(1,1))
+
+// Diferença principal? Delcaração pode ser chamada antes da declaração.
+
+console.log(apresentar())
+
+function apresentar() {
+    return "olá";
+}
+
+
+const soma = function(num1, num2) {
+    return num1 + num2
+}
+console.log(soma(1,1))
