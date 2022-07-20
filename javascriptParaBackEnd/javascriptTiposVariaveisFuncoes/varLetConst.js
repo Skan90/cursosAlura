1c7f29a0fbc6a6517565f13a39bd32c67ebd3add
Rafael da Silva Santos <rafasilva.9021@gmail.com>
2022-07-20 06:03:50 -0300

    Update varLetConst.js

--- javascriptParaBackEnd/javascriptTiposVariaveisFuncoes/varLetConst.js
+++ javascriptParaBackEnd/javascriptTiposVariaveisFuncoes/varLetConst.js
@@ -39,3 +39,17 @@ if (Forma === 'quadrado') {
 
 
 console.log(areaQuadrado)
+
+
+var respostaDeTudo = 42
+let idade = 29
+const pi = 3.14
+
+{
+    var respostaDeTudo = 3.14
+    let idade = 42
+    const pi = 29
+    console.log(respostaDeTudo, idade, pi)
+
+}
+console.log(respostaDeTudo, idade, pi)
