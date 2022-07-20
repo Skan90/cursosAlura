8b48165d5879572373bec0fb091176d107f006ad
Rafael da Silva Santos <rafasilva.9021@gmail.com>
2022-07-20 06:42:06 -0300

    Create ternario.js

---
+++ javascriptParaBackEnd/javascriptTiposVariaveisFuncoes/ternario.js
@@ -0,0 +1,15 @@
+
+//Operador ternário como se fosse if ou if/else
+
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+// if (idadeCliente >= idadeMinima) {
+//     console.log("cerveja")
+// } else {
+//     console.log("suco")
+// }
+                //condição                  //true    //false
+console.log(idadeCliente >= idadeMinima ? "cerveja" : "suco")
+
+// três operadores ">=" e "?" e ":", portanto, ternário.
