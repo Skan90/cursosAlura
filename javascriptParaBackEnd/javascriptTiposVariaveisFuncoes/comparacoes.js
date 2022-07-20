de31385d9eb0331b888164d20abc93584b2da419
Rafael da Silva Santos <rafasilva.9021@gmail.com>
2022-07-20 06:42:09 -0300

    Create comparacoes.js

---
+++ javascriptParaBackEnd/javascriptTiposVariaveisFuncoes/comparacoes.js
@@ -0,0 +1,22 @@
+// == (comparação implícita)
+
+const numero = 5;
+const texto = "5"
+
+console.log(numero == texto) //true 
+console.log(numero === texto) //false 
+
+// usar === como padrão 
+// se utilizar das conversões como boas práticas
+
+Number()
+String()
+
+
+
+// ||: Operador “ou”, retorna true caso uma condição seja válida;
+// &&: Operador “e”, retorna true somente se todas as condições forem válidas;
+// != e !==: Operadores “não igual” e “estritamente não igual”, 
+// utilizados para comparação, da mesma forma que == e === retornam true ou false.
+
+//Documentação: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Expressions_and_operators
