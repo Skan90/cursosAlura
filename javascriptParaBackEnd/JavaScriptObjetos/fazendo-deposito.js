fb877b98de65e7a9927834ac7c09e134209eb63b
Rafael da Silva Santos <rafasilva.9021@gmail.com>
2022-07-21 07:14:42 -0300

    Create fazendo-deposito.js

---
+++ javascriptParaBackEnd/JavaScriptObjetos/fazendo-deposito.js
@@ -0,0 +1,22 @@
+const cliente = {
+    nome: "Rafael",
+    idade: 32,
+    cpf: "11144488852",
+    email: "rafael@email.com",
+    fones: ["5553888888888", 
+    "5553777777777"],
+    dependentes: [
+        {
+            nome: "Sarah",
+            parentesco: "filha",
+            dataNasc: "08/08/2008"
+        },
+        {
+            nome: "Bárbara",
+            parentesco: "esposa",
+            dataNasc: "28//08/1991"
+        }
+    ],
+    saldo: 100,
+    
+};
