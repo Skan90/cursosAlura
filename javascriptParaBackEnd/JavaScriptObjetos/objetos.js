81dbc453f77a5f1f5ab6b95c2413a89e30ad2f29
Rafael da Silva Santos <rafasilva.9021@gmail.com>
2022-07-21 03:42:24 -0300

    Update objetos.js

--- javascriptParaBackEnd/JavaScriptObjetos/objetos.js
+++ javascriptParaBackEnd/JavaScriptObjetos/objetos.js
@@ -8,6 +8,6 @@ const listaCPFs = [ "1155877888", "11144455588", "88888855544"]
 const cliente = {
     nome: "Rafael",
     idade: 32,
-    CPF: "11144488852",
+    cpf: "11144488852",
     email: "rafael@email.com"
 };
