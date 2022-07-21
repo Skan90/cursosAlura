273e4e7637dc79b073096ea5bd3a8c1dde613c7b
Rafael da Silva Santos <rafasilva.9021@gmail.com>
2022-07-21 00:08:41 -0300

    Create matrizes.js

---
+++ javascriptParaBackEnd/JavaScriptArrays/matrizes.js
@@ -0,0 +1,9 @@
+idades = [30, 35, 28];
+nomes = ["Ana", "Juliana", "Leonardo"];
+faculdade = [false, true, true];
+
+
+funcionarios = [nomes,idades,faculdade];
+
+
+console.log(`${funcionarios[0][0]}, tem a idade de ${funcionarios[1][0]} anos.`)
