0603ae01e8bbb28b30e9392c19f839391e8f59f8
Rafael da Silva Santos <rafasilva.9021@gmail.com>
2022-07-21 04:17:00 -0300

    Create spiderMan.js

--- javascriptParaBackEnd/JavaScriptObjetos/adicionarCampos.js
+++ javascriptParaBackEnd/JavaScriptObjetos/adicionarCampos.js
@@ -13,12 +13,12 @@ console.log(cliente);
 
 // Cria campo e adiciona valor
 
-cliente.fone = "53999999999;"
+cliente.fone = "53999999999";
 
 console.log(cliente);
 
 // Após a criação, o valor de campo já existente é apenas alterado.
 
-cliente.fone = "53988119988;"
+cliente.fone = "53988119988";
 
 console.log(cliente);
---
+++ javascriptParaBackEnd/JavaScriptObjetos/spiderMan.js
@@ -0,0 +1,20 @@
+const siperMan = {
+    nome: "Peter Benjamin Parker",
+    idade: 15,
+    altura: "1.78",
+    cidadeNatal: "New York",
+    estado: "NY"
+
+    
+};
+
+console.log(siperMan)
+
+siperMan.mae = "May Parker";
+siperMan.pai = "Richard Parker";
+siperMan.realidadeTerra = "Terra-616";
+siperMan.tio = "Benjamin Parker";
+siperMan.tia = "May Parker-Jameson";
+siperMan.tioAdotivo = "Jay Jameson";
+
+console.log(siperMan)
