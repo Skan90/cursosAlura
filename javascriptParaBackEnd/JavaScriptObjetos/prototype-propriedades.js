50b647e2004a373efa11917102c8f5c0b0a2a247
Rafael da Silva Santos <rafasilva.9021@gmail.com>
2022-07-23 16:03:16 -0300

    Create prototype-propriedades.js

---
+++ javascriptParaBackEnd/JavaScriptObjetos/prototype-propriedades.js
@@ -0,0 +1,25 @@
+// DESAFIO - Cliente genérico
+/*Gerar uma função que permita a criação de
+novos clientes a partir de um modelo*/
+
+function Cliente(nome, cpf, email, saldo)
+{
+    this.nome = nome
+    this.cpf = cpf
+    this.email = email
+    this.saldo = saldo
+    this.depositar = function(valor){
+        this.saldo += valor
+    }
+}
+
+const rafael = new Cliente("Rafael", "1114448884", "rafael@email.com", 100)
+
+console.log(rafael)
+
+// rafael.__proto__
+// rafael.__proto__.__proto__
+//console do navegador trás propriedades não enumeráveis
+//Construtor de objeto, ou seja, o nosso cliente, a nossa função cliente,
+//utilizou o construtor objeto, e a nossa instância andre utilizou o construtor cliente,
+/// chamamos isso de cadeia de protótipo
