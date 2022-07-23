d21a9689a5484598134734c0e2f0c6c9987069ac
Rafael da Silva Santos <rafasilva.9021@gmail.com>
2022-07-23 15:21:35 -0300

    Create lista-unica.js

---
+++ javascriptParaBackEnd/JavaScriptObjetos/lista-unica.js
@@ -0,0 +1,40 @@
+const clientes = [
+	{
+		nome: "Rafael",
+		idade: 32,
+		cpf: "11144488852",
+		email: "rafael@email.com",
+		fones: ["5553888888888", 
+		"5553777777777"],
+		dependentes: [
+			{
+				nome: "Sarah",
+				parentesco: "filha",
+				dataNasc: "08/08/2008"
+			},
+			{
+				nome: "Bárbara",
+				parentesco: "esposa",
+				dataNasc: "28//08/1991"
+			}]
+		},
+		{
+			nome: "Ana Luisa",
+			idade: 62,
+			cpf: "11144488852",
+			email: "anal@email.com",
+			fones: ["5553888888888", 
+			"5553777777788"],
+			dependentes: [
+				{
+					nome: "Tulee",
+					parentesco: "Pet",
+					dataNasc: "08/09/2008"
+				}
+			]
+		}
+	]
+
+	const listaDependentes = [...clientes[0].dependentes, ...clientes[1].dependentes];
+	console.log(listaDependentes)
+	console.table(listaDependentes)
