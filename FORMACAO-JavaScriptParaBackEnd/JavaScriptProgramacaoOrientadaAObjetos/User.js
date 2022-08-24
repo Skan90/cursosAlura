a518fb5027eadff192cb846afdc6ef74f885b672
Rafael da Silva Santos <rafasilva.9021@gmail.com>
2022-08-24 00:54:09 -0300

    Conceitos sobre encapsulamento (getters e setters)
    Exemplos e notas para conceitos de encapsulamento através de getters e setters. Alguns usos de boas práticas (um get para cada atribuição) ao escrever código.

--- FORMACAO-JavaScriptParaBackEnd/JavaScriptProgramacaoOrientadaAObjetos/User.js
+++ FORMACAO-JavaScriptParaBackEnd/JavaScriptProgramacaoOrientadaAObjetos/User.js
@@ -1,16 +1,74 @@
 
 //ecmascript module ESM
 export default class User {
+
+    /*Atributos privados (adicionados com cerquilha e declarados antes do constructor) 
+    servem para impedir o acesso de outras partes do código a um determinado dado, 
+    no caso uma propriedade, ou pode ser um método que não pode ser acessado também, 
+    por fora da classe, só por dentro da classe. Os métodos dentro da classe 
+    conseguem acessar as propriedades da classe. Exceção: Atributos expostos*/
+
+    #nome
+    #email
+    #nascimento
+    #role
+    #ativo
     constructor(nome, email, nascimento, role, ativo = true){
-        this.nome = nome
-        this.email = email
-        this.nascimento = nascimento
-        this.role = role || 'estudante'
-        this.ativo = ativo
+        this.#nome = nome
+        this.#email = email
+        this.#nascimento = nascimento
+        this.#role = role || 'estudante'
+        this.#ativo = ativo
     }
     
+    // get é uma propriedade readOnly
+    get nome() {
+        // get permite lógica condicional (if, else etc)
+        // porém na chamada não precisa de "()" parênteses.
+        // Usa-se a sintaxe de própriedade
+        return this.#nome
+    }
+
+    get email() {
+        return this.#email
+    }
+
+
+    get nascimento() {
+        return this.#nascimento
+    }
+
+
+    get role() {
+        return this.#role
+    }
+
+
+    get ativo() {
+        return this.#ativo
+    }
+
+    set nome(novoNome){
+        if(novoNome === ''){
+            throw new Error('formato não válido')
+        }
+        this.#nome = novoNome
+    }
+    // Boas práticas, cada get com sua única função definida.
+
+    // #montathis() {
+    //     return({
+    //         nome: this.#nome,
+    //         email: this.#email,
+    //         nascimento: this.#nascimento,
+    //         role: this.#role,
+    //         ativo: this.#ativo
+    //     })
+    // }
+
     exibirInfos(){
-        return `${this.nome}, ${this.email}`
+        // const objUser = this.#montaObjUser()
+        return `${this.nome}, ${this.email}, ${this.nascimento}, ${this.role}, ${this.ativo}`
     }
 }
 
--- FORMACAO-JavaScriptParaBackEnd/JavaScriptProgramacaoOrientadaAObjetos/index.js
+++ FORMACAO-JavaScriptParaBackEnd/JavaScriptProgramacaoOrientadaAObjetos/index.js
@@ -2,5 +2,10 @@ import User from "./User.js";
 import Docente from "./Docente.js";
 import Admin from "./Admin.js";
 
-const novoUser = new User('Rafael', 'r@r.com', '2022-08-28');
-console.log(novoUser.exibirInfos());
+// const novoUser = new User('Rafael', 'r@r.com', '2022-08-28');
+// console.log(novoUser.exibirInfos());
+
+const novoAdmin = new Admin('Luis', 'l@l.com', '2022-08-24');
+console.log(novoAdmin.nome);
+novoAdmin.nome = 'Ana'; //sintaxe de propriedade
+console.log(novoAdmin.nome);
